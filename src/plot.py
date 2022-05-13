650555b16e3b6dc1475b8fb645de595ea1b3a3cc
Robotics Student <student@TENB885849C7856.shefuniad.shef.ac.uk>
2022-05-13 14:33:34 +0100

    rush

--- src/plot.py
+++ src/plot.py
@@ -3,19 +3,45 @@ from joblib import load
 import os
 import sys
 
-index = int(sys.argv[1])    
+"""
+open
+    random
+    full
+closed
+    random
+    full
+"""
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+if sys.argv == 'diff':
+    for i in range(0, 12, 3):
+        diff1 = data[i][-1] - diff[i][0]
+        diff2 = data[i+1][-1] - diff[i+1][0]
+        diff3 = data[i+2][-1] - diff[i+2][0]
+        print(diff1, diff2, diff3, (diff1+diff2+diff3)/3)
+else:
 
-data = load(dir_path + '/results.gz')
+    index = int(sys.argv[1])    
 
-print(len(data))
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
-plt.figure(figsize=(12, 8))
+    data = load(dir_path + '/all_results.gz')
 
-time = data[index][:,0]
-pos = data[index][:,1]
+    print(len(data))
 
-plt.plot(time, pos)
+    plt.figure(figsize=(12, 8))
 
-plt.show()
+    # if data[index].shape[0] > 0:
+    #     print(data[index][0])
+    #     print(data[index][1])
+    #     print(data[index][2])
+    #     print(data[index][3])
+    #     print(data[index][4])
+
+    time = data[index][:,0]
+    pos = data[index][:,1]
+
+    plt.plot(time, pos)
+    plt.xlabel('time (s)')
+    plt.ylabel('average distance (m)')
+
+    plt.show()
